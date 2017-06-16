e8eac7796fa16f5ab3309f933708b75ff2a6c8bd
Pedro Nogueira <pedrohnog@users.noreply.github.com>
2017-06-16 14:40:23 -0300

    Criados primeiros comandos no ContaComando
    Adicionados alguns tratamentos no ContaComando

--- Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/banco/comandos/ContaComando.java
+++ Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/banco/comandos/ContaComando.java
@@ -1,31 +1,73 @@
 package br.com.fiap.banco.comandos;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import br.com.fiap.banco.constantes.TipoUsuario;
+import br.com.fiap.banco.dao.impl.ContaDao;
+import br.com.fiap.banco.dao.impl.UsuarioDao;
+import br.com.fiap.banco.entidades.Conta;
 import br.com.fiap.banco.entidades.Usuario;
 
 public class ContaComando {
-	
-	public boolean temConta(long id) {
-		//TODO Verificar no BD se o ID existe
-		return true;
+
+	public boolean temConta(long idTelegram) {
+		try (ContaDao contaDao = new ContaDao();) {
+			return contaDao.temConta(idTelegram);
+		}
 	}
-	
-	public void criarConta(long id, String nome, String sobrenome, String telefone, String cpf, String email) {
-		//TODO Persistir as informações do cliente no BD
+
+	public void criarConta(long idTelegram, String nome, String sobrenome, String telefone, String cpf, String email) {
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome + " " + sobrenome);
+		usuario.setTelefone(telefone);
+		usuario.setCpf(cpf);
+		usuario.setEmail(email);
+		usuario.setTipoUsuario(TipoUsuario.PRINCIPAL.getCodigo());
+
+		Conta conta = new Conta();
+		conta.setNumero(idTelegram);
+		conta.setDataAbertura(LocalDate.now());
+		conta.setSaldo(0.0d);
+
+		usuario.setConta(conta);
+
+		try (UsuarioDao usuarioDao = new UsuarioDao(); ContaDao contaDao = new ContaDao();) {
+			if (contaDao.criarConta(conta)) {
+				usuarioDao.criarUsuario(usuario);
+			}
+		}
 	}
-	
-	public void alterarConta(long id, String cpf, String email) {
-		//TODO Atualizar o BD com as novas informações
+
+	public void alterarConta(long idTelegram, String cpf, String email) {
+		// TODO Descobrir qual o usuário que precisa ser alterado
 	}
-	
-	public void incluirDependente(long id, String nome, String sobrenome, String telefone, String cpf, String email) {
-		//XXX Definir como vai ter todas as informações do dependente como o ID, por exemplo!!!
+
+	public void incluirDependente(long idTelegram, String nome, String sobrenome, String telefone, String cpf,
+			String email) {
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome + " " + sobrenome);
+		usuario.setTelefone(telefone);
+		usuario.setCpf(cpf);
+		usuario.setEmail(email);
+		usuario.setTipoUsuario(TipoUsuario.DEPENDENTE.getCodigo());
+
+		try (UsuarioDao usuarioDao = new UsuarioDao(); ContaDao contaDao = new ContaDao();) {
+			if (usuarioDao.buscarUsuario(cpf) == null) {
+				Conta conta = contaDao.buscarConta(idTelegram);
+
+				usuario.setConta(conta);
+
+				usuarioDao.criarUsuario(usuario);
+			}
+		}
+
 	}
-	
-	public List<Usuario> listarUsuarios(long id) {
-		//TODO Listar todos os usuários e dependentes com base na conta informada
-		return null;
+
+	public List<Usuario> listarUsuarios(long idTelegram) {
+		try (ContaDao contaDao = new ContaDao();) {
+			return contaDao.buscarConta(idTelegram).getUsuarios();
+		}
 	}
 
 }
