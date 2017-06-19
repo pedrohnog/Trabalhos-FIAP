7d57b9a49e8fffd3720c96a84e7cd5a6bca6dbfb
Pedro Nogueira <pedrohnog@users.noreply.github.com>
2017-06-19 18:10:24 -0300

    Criação de get para a constante Tipo de Transação

--- Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/banco/constantes/TipoTransacao.java
+++ Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/banco/constantes/TipoTransacao.java
@@ -2,11 +2,7 @@ package br.com.fiap.banco.constantes;
 
 public enum TipoTransacao {
 
-	SAQUE		(1),
-	DEPOSITO	(2),
-	TARIFA		(3),
-	EMPRESTIMO	(4),
-	JUROS		(5);
+	SAQUE(1), DEPOSITO(2), TARIFA(3), EMPRESTIMO(4), JUROS(5);
 
 	private int codigo;
 
@@ -16,6 +12,17 @@ public enum TipoTransacao {
 
 	public int getCodigo() {
 		return codigo;
+	}
+
+	public static TipoTransacao getTipoTransacao(int codigo) {
+		if (codigo > 0) {
+			for (TipoTransacao tipoTransacao : values()) {
+				if (tipoTransacao.codigo == codigo) {
+					return tipoTransacao;
+				}
+			}
+		}
+		return null;
 	}
 
 }
