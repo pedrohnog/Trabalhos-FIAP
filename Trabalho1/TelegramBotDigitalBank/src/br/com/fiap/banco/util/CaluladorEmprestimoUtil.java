0f84998a92dfe200c49b4483b3dc3ad2b90d6a63
Pedro Nogueira <pedrohnog@users.noreply.github.com>
2017-06-19 21:51:26 -0300

    Finalizado calculadora de parcelas

--- Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/banco/util/CaluladorEmprestimoUtil.java
+++ Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/banco/util/CaluladorEmprestimoUtil.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.fiap.banco.constantes.Tarifas;
+import br.com.fiap.banco.entidades.Conta;
 import br.com.fiap.banco.entidades.Emprestimo;
 
 public class CaluladorEmprestimoUtil {
 
-	public List<Emprestimo> calcularEmprestimo(double valor, int qtdeParcelas) {
+	public static List<Emprestimo> calcularEmprestimo(Conta conta, double valor, int qtdeParcelas) {
 		List<Emprestimo> parcelas = new ArrayList<>();
 
 		double valorParcela = valor / qtdeParcelas;
@@ -17,9 +18,11 @@ public class CaluladorEmprestimoUtil {
 		for (int i = 1; i <= qtdeParcelas; i++) {
 			Emprestimo emprestimo = new Emprestimo();
 
+			emprestimo.setConta(conta);
+			emprestimo.setParcelaPaga(false);
+			
 			emprestimo.setNumeroParcela(i);
 			emprestimo.setDataVencimento(LocalDate.now().plusMonths(i));
-			emprestimo.setParcelaPaga(false);
 
 			valorParcela += valorParcela * ((double) Tarifas.EMPRESTIMO.getJurosMensais() / 100);
 
@@ -29,15 +32,6 @@ public class CaluladorEmprestimoUtil {
 		}
 
 		return parcelas;
-	}
-
-	public static void main(String[] args) {
-		CaluladorEmprestimoUtil caluladorEmprestimoUtil = new CaluladorEmprestimoUtil();
-		List<Emprestimo> lista = caluladorEmprestimoUtil.calcularEmprestimo(5000, 20);
-
-		for (Emprestimo emprestimoDetalhe : lista) {
-			System.out.println(emprestimoDetalhe.getValorParcela());
-		}
 	}
 
 }
