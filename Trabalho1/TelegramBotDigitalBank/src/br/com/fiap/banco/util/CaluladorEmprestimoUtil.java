8c0d0d0d3e4763811e9ac33ce40f72858290cbd0
Pedro Nogueira <pedrohnog@users.noreply.github.com>
2017-06-19 22:33:59 -0300

    Corrigido valores do empréstimo para duas casas decimais

--- Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/banco/util/CaluladorEmprestimoUtil.java
+++ Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/banco/util/CaluladorEmprestimoUtil.java
@@ -1,14 +1,19 @@
 package br.com.fiap.banco.util;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import br.com.fiap.banco.constantes.Tarifas;
 import br.com.fiap.banco.entidades.Conta;
 import br.com.fiap.banco.entidades.Emprestimo;
 
 public class CaluladorEmprestimoUtil {
+	
+	private static DecimalFormat df2 = new DecimalFormat(".##", DecimalFormatSymbols.getInstance(new Locale("en", "US")));
 
 	public static List<Emprestimo> calcularEmprestimo(Conta conta, double valor, int qtdeParcelas) {
 		List<Emprestimo> parcelas = new ArrayList<>();
@@ -26,7 +31,7 @@ public class CaluladorEmprestimoUtil {
 
 			valorParcela += valorParcela * ((double) Tarifas.EMPRESTIMO.getJurosMensais() / 100);
 
-			emprestimo.setValorParcela(valorParcela);
+			emprestimo.setValorParcela(Double.valueOf(df2.format(valorParcela)));
 
 			parcelas.add(emprestimo);
 		}
