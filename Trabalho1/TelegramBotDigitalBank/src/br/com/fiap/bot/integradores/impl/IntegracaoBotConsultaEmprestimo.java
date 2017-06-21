8b8c73dddb11c97d3ca72662b3c24fee225cd824
Rafael Alves de Lima <rafael.alvesdelima89@gmail.com>
2017-06-21 18:41:14 -0300

    Implementação da integração com o banco na classe IntegracaoBotConsultaEmprestimo
    Implementação da integração com o banco na classe IntegracaoBotConsultaEmprestimo

--- Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/integradores/impl/IntegracaoBotConsultaEmprestimo.java
+++ Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/integradores/impl/IntegracaoBotConsultaEmprestimo.java
@@ -1,19 +1,60 @@
 package br.com.fiap.bot.integradores.impl;
 
+import java.util.List;
+
 import com.pengrad.telegrambot.model.Chat;
 
+import br.com.fiap.banco.comandos.BotComando;
+import br.com.fiap.banco.entidades.Emprestimo;
 import br.com.fiap.bot.integradores.IntegracaoBotConsulta;
+import br.com.fiap.bot.util.DataUtil;
+import br.com.fiap.bot.util.MoedaUtil;
 
 public class IntegracaoBotConsultaEmprestimo extends IntegracaoBotConsulta {
 
 	@Override
 	public String integrarBanco(String resposta, Chat usuario) {
-		return "EMPRESTIMO";
+		BotComando botComando =  new BotComando();
+		StringBuffer retorno = new StringBuffer();
+		List<Emprestimo> emprestimosNaoPagos = botComando.listarEmprestimosNaoPagos(usuario.id());
+		List<Emprestimo> emprestimosVencidos = botComando.listarEmprestimosVencidos(usuario.id());
+		
+		if(!emprestimosNaoPagos.isEmpty() || !emprestimosVencidos.isEmpty()){
+			
+			if(!emprestimosVencidos.isEmpty()){
+				retorno.append("PARCELAS VENCIDAS");
+				retorno.append("\n\n");
+				for(Emprestimo parcelas : emprestimosVencidos){
+					retorno.append(parcelas.getNumeroParcela()).append(" - ")
+					.append(DataUtil.conveterDataPadraoBr(parcelas.getDataVencimento())).append(" - ")
+					.append(MoedaUtil.conveterMoedaBr(parcelas.getValorParcela()))
+					.append("\n");
+				}
+				retorno.append("\n\n");
+			}
+		
+			if(!emprestimosNaoPagos.isEmpty()){
+				retorno.append("PARCELAS NAO PAGAS");
+				retorno.append("\n\n");
+				for(Emprestimo parcelas : emprestimosNaoPagos){
+					retorno.append(parcelas.getNumeroParcela()).append(" - ")
+					.append(DataUtil.conveterDataPadraoBr(parcelas.getDataVencimento())).append(" - ")
+					.append(MoedaUtil.conveterMoedaBr(parcelas.getValorParcela()))
+					.append("\n");
+				}
+				retorno.append("\n\n");
+			}
+			
+		}else{
+			retorno.append("Você não tem emprestimo para ser pago!");
+		}
+		
+		return retorno.toString();
 	}
 
 	@Override
 	public String tratarPrimeiraInteracao(Chat usuario) {
-		return "EMPRESTIMO";
+		return this.integrarBanco("", usuario);
 	}
 
 }
