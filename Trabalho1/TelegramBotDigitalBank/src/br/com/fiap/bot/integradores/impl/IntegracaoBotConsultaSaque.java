48d116d34eba462458449604db54e136fd969f4d
vcoimbra <victorcoimbra2@hotmail.com>
2017-06-21 17:57:47 -0300

    Refatorando o codigo para usar o converte do dataUtil
    Refatorando o codigo para usar o converte do dataUtil

--- Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/integradores/impl/IntegracaoBotConsultaSaque.java
+++ Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/integradores/impl/IntegracaoBotConsultaSaque.java
@@ -1,9 +1,5 @@
 package br.com.fiap.bot.integradores.impl;
 
-import java.text.NumberFormat;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
 import com.pengrad.telegrambot.model.Chat;
 
 import br.com.fiap.banco.comandos.BotComando;
@@ -12,6 +8,7 @@ import br.com.fiap.banco.entidades.Transacao;
 import br.com.fiap.banco.excecao.ContaInexistenteExcecao;
 import br.com.fiap.bot.integradores.IntegracaoBotConsulta;
 import br.com.fiap.bot.util.ConverteMoedaUtil;
+import br.com.fiap.bot.util.DataUtil;
 
 public class IntegracaoBotConsultaSaque extends IntegracaoBotConsulta {
 
@@ -20,14 +17,13 @@ public class IntegracaoBotConsultaSaque extends IntegracaoBotConsulta {
 		BotComando botComando = new BotComando();
 		TransacaoDetalhe transacaoDetalhe;
 		StringBuilder retorno = new StringBuilder();
-		NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
 		
 		try {
 			transacaoDetalhe = botComando.listarRetiradas(usuario.id());
 			retorno.append("EXTRATO DE SAQUES \n");
 			for (Transacao transacao : transacaoDetalhe.getTransacoes()) {
-				retorno.append(transacao.getDataHora().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
-						+ ":" + ConverteMoedaUtil.conveterMoedaBr(transacao.getValor()) + "\n");
+				retorno.append(DataUtil.conveterDataPadraoBr(transacao.getDataHora())
+						+ ": " + ConverteMoedaUtil.conveterMoedaBr(transacao.getValor()) + "\n");
 			}
 			retorno.append("Total: " + transacaoDetalhe.getSomatorio());
 		} catch (ContaInexistenteExcecao e) {
