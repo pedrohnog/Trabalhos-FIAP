ca631922a124b22726796465862221c78637218c
Rafael Alves de Lima <rafael.alvesdelima89@gmail.com>
2017-06-21 18:40:21 -0300

    Inclusão de sobrecarga no método conveterDataPadraoBr para LocalDate
    Inclusão de sobrecarga no método conveterDataPadraoBr para LocalDate

--- Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/util/DataUtil.java
+++ Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/util/DataUtil.java
@@ -1,5 +1,6 @@
 package br.com.fiap.bot.util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -7,6 +8,9 @@ public class DataUtil {
 	
 	public static String conveterDataPadraoBr(LocalDateTime localDateTime){
 		return localDateTime.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+	}
+	public static String conveterDataPadraoBr(LocalDate localDate){
+		return localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 	}
 
 }
