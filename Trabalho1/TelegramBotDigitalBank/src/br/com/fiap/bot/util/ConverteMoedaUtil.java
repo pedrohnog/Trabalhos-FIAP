5eb20a514342c24b71516a3fa3cd3d574428d072
vcoimbra <victorcoimbra2@hotmail.com>
2017-06-21 17:29:28 -0300

    Criação do metodo para converter o padrão de moeda
    Criação do metodo para converter o padrão de moeda

---
+++ Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/util/ConverteMoedaUtil.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bot.util;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class ConverteMoedaUtil {
+	
+	public static String conveterMoedaBr(double valor){
+		NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		return format.format(valor);
+	}
+
+}
