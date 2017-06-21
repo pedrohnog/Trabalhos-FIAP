7b92b0137797b14728c222c67febf3f713cec175
vcoimbra <victorcoimbra2@hotmail.com>
2017-06-21 18:26:41 -0300

    Implementação do Deposito
    Implementação do Deposito

--- Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/integradores/impl/IntegracaoBotConsultaDeposito.java
+++ Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/integradores/impl/IntegracaoBotConsultaDeposito.java
@@ -2,12 +2,39 @@ package br.com.fiap.bot.integradores.impl;
 
 import com.pengrad.telegrambot.model.Chat;
 
+import br.com.fiap.banco.comandos.BotComando;
+import br.com.fiap.banco.constantes.TipoTransacao;
+import br.com.fiap.banco.dados.TransacaoDetalhe;
+import br.com.fiap.banco.entidades.Transacao;
+import br.com.fiap.banco.excecao.ContaInexistenteExcecao;
 import br.com.fiap.bot.integradores.IntegracaoBotConsulta;
+import br.com.fiap.bot.util.DataUtil;
+import br.com.fiap.bot.util.MoedaUtil;
 
 public class IntegracaoBotConsultaDeposito extends IntegracaoBotConsulta {
 
 	@Override
 	public String integrarBanco(String resposta, Chat usuario) {
+		BotComando botComando = new BotComando();
+		TransacaoDetalhe transacaoDetalhe;
+		StringBuilder retorno = new StringBuilder();
+		
+		try {
+			transacaoDetalhe = botComando.listarLancamentos(usuario.id());
+			retorno.append("EXTRATO DE SAQUES \n");
+			for (Transacao transacao : transacaoDetalhe.getTransacoes()) {
+				retorno.append(DataUtil.conveterDataPadraoBr(transacao.getDataHora())
+						+ " - " + TipoTransacao.getTipoTransacao(transacao.getTipoTransacao()).toString()
+						+ ": " + MoedaUtil.conveterMoedaBr(transacao.getValor()) + "\n");
+			}
+			retorno.append("Total: " + MoedaUtil.conveterMoedaBr(transacaoDetalhe.getSomatorio()));
+			retorno.append("Você ainda não tem uma conta, para criar sua conta digite /criar_conta"); 
+			
+		} catch (ContaInexistenteExcecao e) {
+			
+		}
+			
+		
 		return "DEPOSITO";
 	}
 
