297f197792608f1cf66547ba72fa723ea38e7d98
Pedro Nogueira <pedrohnog@users.noreply.github.com>
2017-06-27 13:41:15 -0300

    Removido BotUtil

--- Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/Bot.java
+++ Trabalho1/TelegramBotDigitalBank/src/br/com/fiap/bot/Bot.java
@@ -1,5 +1,6 @@
 package br.com.fiap.bot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,17 +8,20 @@ import java.util.Map;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.TelegramBotAdapter;
 import com.pengrad.telegrambot.model.Chat;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ChatAction;
 import com.pengrad.telegrambot.request.GetUpdates;
 import com.pengrad.telegrambot.request.SendChatAction;
 import com.pengrad.telegrambot.request.SendMessage;
 
+import br.com.fiap.bot.constantes.ConstantesBot;
 import br.com.fiap.bot.constantes.EnumComandosBot;
 import br.com.fiap.bot.constantes.EnumTipoIntegracaoBot;
+import br.com.fiap.bot.dao.impl.MensagemIntegracaoDao;
+import br.com.fiap.bot.entidades.MensagemIntegracao;
 import br.com.fiap.bot.integradores.IntegracaoBot;
 import br.com.fiap.bot.integradores.IntegracaoBotSolicitacao;
-import br.com.fiap.bot.util.BotUtil;
 import br.com.fiap.bot.util.PropriedadesUtil;
 
 /**
@@ -76,16 +80,16 @@ public class Bot {
 		Chat usuario = update.message().chat();
 		String mensagemRecebida = update.message().text();
 
-		if (BotUtil.isMensagemComComando(update.message())) {
+		if (isMensagemComComando(update.message())) {
 			mensagemRetorno = tratarMensagemBotComComando(usuario, mensagemRecebida);
-		} else if (BotUtil.botAguardandoRespostaDoUsuario(usuario)) {
+		} else if (botAguardandoRespostaDoUsuario(usuario)) {
 			mensagemRetorno = tratarRespostaEnviada(usuario, mensagemRecebida);
 		} else {
 			mensagemRetorno = "Desculpe, não entendi... digite /ajuda para obter a lista de comandos conhecidos.";
 
 		}
 
-		BotUtil.salvarMensagens(usuario, mensagemRecebida, mensagemRetorno);
+		salvarMensagens(usuario, mensagemRecebida, mensagemRetorno);
 
 		return mensagemRetorno;
 	}
@@ -100,11 +104,11 @@ public class Bot {
 	 */
 	private String tratarRespostaEnviada(Chat usuario, String mensagemRecebida) {
 		String mensagemRetorno;
-		IntegracaoBotSolicitacao resposta = (IntegracaoBotSolicitacao) BotUtil.definirClasseIntegracao(ULTIMO_COMANDO_DO_USUARIO.get(usuario.id()));
+		IntegracaoBotSolicitacao resposta = (IntegracaoBotSolicitacao) definirClasseIntegracao(ULTIMO_COMANDO_DO_USUARIO.get(usuario.id()));
 
 		if (resposta.validarResposta(mensagemRecebida)) {
 			mensagemRetorno = resposta.integrarBanco(mensagemRecebida, usuario);
-			BotUtil.removerHistoricoIntegracaoUsuario(usuario.id());
+			removerHistoricoIntegracaoUsuario(usuario.id());
 		} else {
 			mensagemRetorno = resposta.informarErroNaResposta();
 		}
@@ -122,17 +126,17 @@ public class Bot {
 	private String tratarMensagemBotComComando(Chat usuario, String mensagemRecebida) {
 		String mensagemRetorno;
 
-		if (BotUtil.isComandoSimples(mensagemRecebida)) {
-			IntegracaoBot resposta = BotUtil.definirClasseIntegracao(EnumComandosBot.LISTA_COMANDO_INTERACOES.get(mensagemRecebida));
+		if (isComandoSimples(mensagemRecebida)) {
+			IntegracaoBot resposta = definirClasseIntegracao(EnumComandosBot.LISTA_COMANDO_INTERACOES.get(mensagemRecebida));
 			mensagemRetorno = resposta.tratarPrimeiraInteracao(usuario);
 
 			if (EnumComandosBot.LISTA_COMANDO_INTERACOES.get(mensagemRecebida).getEnumTipoIntegracaoBot().equals(EnumTipoIntegracaoBot.SOLICITACAO)) {
-				BotUtil.adicionarHistoricoIntegracaoUsuario(usuario.id(), EnumComandosBot.LISTA_COMANDO_INTERACOES.get(mensagemRecebida));
+				adicionarHistoricoIntegracaoUsuario(usuario.id(), EnumComandosBot.LISTA_COMANDO_INTERACOES.get(mensagemRecebida));
 			} else {
-				BotUtil.removerHistoricoIntegracaoUsuario(usuario.id());
+				removerHistoricoIntegracaoUsuario(usuario.id());
 			}
 		} else {
-			List<String> comandosEnviados = BotUtil.encontrarComandosNaMensagem(mensagemRecebida);
+			List<String> comandosEnviados = encontrarComandosNaMensagem(mensagemRecebida);
 
 			if (comandosEnviados.isEmpty()) {
 				mensagemRetorno = PropriedadesUtil.carregarMensagensIntegracao().getProperty("COMANDO_DESCONHECIDO");
@@ -140,24 +144,158 @@ public class Bot {
 				mensagemRetorno = PropriedadesUtil.carregarMensagensIntegracao().getProperty("COMANDO_DUPLICADO");
 			} else {
 				String comando = comandosEnviados.get(0);
-				String mensagemSemComando = BotUtil.retirarComandoDaMensagem(mensagemRecebida, comando);
+				String mensagemSemComando = retirarComandoDaMensagem(mensagemRecebida, comando);
 
 				if (EnumTipoIntegracaoBot.SOLICITACAO.equals(EnumComandosBot.LISTA_COMANDO_INTERACOES.get(comando).getEnumTipoIntegracaoBot())) {
-					IntegracaoBotSolicitacao resposta = (IntegracaoBotSolicitacao) BotUtil.definirClasseIntegracao(EnumComandosBot.LISTA_COMANDO_INTERACOES.get(comando));
+					IntegracaoBotSolicitacao resposta = (IntegracaoBotSolicitacao) definirClasseIntegracao(EnumComandosBot.LISTA_COMANDO_INTERACOES.get(comando));
 					if (resposta.validarResposta(mensagemSemComando)) {
 						mensagemRetorno = resposta.integrarBanco(mensagemSemComando, usuario);
-						BotUtil.removerHistoricoIntegracaoUsuario(usuario.id());
+						removerHistoricoIntegracaoUsuario(usuario.id());
 					} else {
 						mensagemRetorno = resposta.informarErroNaResposta();
-						BotUtil.adicionarHistoricoIntegracaoUsuario(usuario.id(), EnumComandosBot.LISTA_COMANDO_INTERACOES.get(comando));
+						adicionarHistoricoIntegracaoUsuario(usuario.id(), EnumComandosBot.LISTA_COMANDO_INTERACOES.get(comando));
 					}
 				} else {
-					IntegracaoBot resposta = BotUtil.definirClasseIntegracao(EnumComandosBot.LISTA_COMANDO_INTERACOES.get(comando));
+					IntegracaoBot resposta = definirClasseIntegracao(EnumComandosBot.LISTA_COMANDO_INTERACOES.get(comando));
 					mensagemRetorno = resposta.tratarPrimeiraInteracao(usuario);
 				}
 			}
 		}
 		return mensagemRetorno;
+	}
+	
+	/**
+	 * Retira o comando que foi recebido na mensagem
+	 * 
+	 * @param mensagemRecebida Mensagem recebida
+	 * @param comando Comando que deve ser removido
+	 * 
+	 * @return Mensagem recebida sem o comando
+	 */
+	public static String retirarComandoDaMensagem(String mensagemRecebida, String comando) {
+		return mensagemRecebida.replaceAll(comando, "");
+	}
+
+	/**
+	 * Encontra todos os comandos conhecidos pelo bot enviados pelo usuário
+	 * 
+	 * @param mensagemRecebida Mensagem enviada pelo usuário
+	 * 
+	 * @return Lista com os comandos encontrados
+	 */
+	public static List<String> encontrarComandosNaMensagem(String mensagemRecebida) {
+		String[] palavras = mensagemRecebida.split(" ");
+		List<String> comandosEncontratos = new ArrayList<>();
+		for (int i = 0; i < palavras.length; i++) {
+			if (palavras[i].startsWith("/") && EnumComandosBot.LISTA_COMANDO_INTERACOES.containsKey(palavras[i].trim())) {
+				comandosEncontratos.add(palavras[i].trim());
+			}
+		}
+		return comandosEncontratos;
+	}
+
+	/**
+	 * Valida se a mensagem recebida é apenas o comando solicitado (ex: /criar_conta)
+	 * 
+	 * @param mensagemRecebida Mensagem enviada pelo usuário no Telegram
+	 * 
+	 * @return <code>true</code> se a mensagem for um comando simples, se não <code>false</code>
+	 */
+	public static boolean isComandoSimples(String mensagemRecebida) {
+		return EnumComandosBot.LISTA_COMANDO_INTERACOES.containsKey(mensagemRecebida);
+	}
+
+	/**
+	 * Valida se o bot esta aguardando uma resposta do usuário
+	 * 
+	 * @param usuario Usuário do Telegram
+	 * 
+	 * @return <code>true</code> se estiver aguardando uma resposta, se não <code>false</code>
+	 */
+	public static boolean botAguardandoRespostaDoUsuario(Chat usuario) {
+		return ULTIMO_COMANDO_DO_USUARIO.containsKey(usuario.id());
+	}
+
+	/**
+	 * Valida se no comando enviado contém uma entidade do tipo "bot_command"
+	 * 
+	 * @param message Objeto Message do Telegram
+	 * 
+	 * @return <code>true</code> se na mensagem contém pelo menos um comando, se não <code>false</code>
+	 */
+	public static boolean isMensagemComComando(Message message) {
+		boolean mensagemComComando = false;
+
+		if (message.entities() != null) {
+			for (int i = 0; i < message.entities().length; i++) {
+				if (message.entities()[i].type().name().equals(ConstantesBot.BOT_COMMAND)) {
+					mensagemComComando = true;
+					break;
+				}
+			}
+		}
+
+		return mensagemComComando;
+	}
+
+	/**
+	 * Método responsável por definir qual será a classe de validação e integração conforme os comandos
+	 * 
+	 * @param EnumComandosBot Comando que foi executado
+	 * 
+	 * @return Instância da classe de acordo com o comando executado
+	 */
+	public static IntegracaoBot definirClasseIntegracao(EnumComandosBot ultimoComandoExecutado) {
+		IntegracaoBot retorno = null;
+		
+		try {
+			return (IntegracaoBot) ultimoComandoExecutado.getClasseComando().newInstance();
+		} catch (SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		
+		return retorno;
+	}
+
+	/**
+	 * Método responsável por remover os comandos do histórico do usuário
+	 * 
+	 * @param id Id do Telegram
+	 */
+	public static void removerHistoricoIntegracaoUsuario(Long id) {
+		ULTIMO_COMANDO_DO_USUARIO.remove(id);
+	}
+
+	/**
+	 * Método responsável por adicionar ou atualizar o último comando executado pelo usuário
+	 * 
+	 * @param id Id do Telegram
+	 * @param comando Comando que será adicionado/atualizado no histório do usuário
+	 */
+	public static void adicionarHistoricoIntegracaoUsuario(Long id, EnumComandosBot comando) {
+		ULTIMO_COMANDO_DO_USUARIO.put(id, comando);
+	}
+
+	/**
+	 * Metodo responsável persistir no BD todas as interações do usuário
+	 * 
+	 * @param usuario Id do Telegram
+	 * @param mensagemRecebida Mensagem enviada pelo usuário para o Bot
+	 * @param mensagemEnviada Mensagem enviada pelo Bot para o usuário
+	 */
+	public static void salvarMensagens(Chat usuario, String mensagemRecebida, String mensagemEnviada) {
+		MensagemIntegracao mensagemIntegracao = new MensagemIntegracao();
+		MensagemIntegracaoDao mensagemIntegracaoDao = new MensagemIntegracaoDao();
+
+		String nomeUsuario = usuario.firstName().replaceAll("[^A-Za-z0-9]", "");
+		
+		mensagemIntegracao.setIdTelegram(usuario.id());
+		mensagemIntegracao.setNomeTelegram(nomeUsuario);
+		mensagemIntegracao.setMensagemRecebida(mensagemRecebida);
+		mensagemIntegracao.setMensagemEnviada(mensagemEnviada);
+
+		mensagemIntegracaoDao.adicionar(mensagemIntegracao);
+		mensagemIntegracaoDao.close();
 	}
 	
 }
