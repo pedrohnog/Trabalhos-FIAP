5475477d7c3ab21096c7da3bb0df98f722f96709
Pedro Nogueira <pedrohnog@users.noreply.github.com>
2017-08-21 22:37:58 -0300

    Criado esqueleto do UsuarioMB

---
+++ Trabalho3/Netgifx/src/br/com/fiap/managedbeans/UsuarioMB.java
@@ -0,0 +1,14 @@
+package br.com.fiap.managedbeans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class UsuarioMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
