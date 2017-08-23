32bb2ae6572035b326fa5ccaccf118b2f7707084
evertonsouzam <evertonsouzam@gmail.com>
2017-08-22 23:13:18 -0300

    correcao para Validar Se usuario já existe

--- Trabalho3/Netgifx/src/br/com/fiap/dao/impl/UsuarioDao.java
+++ Trabalho3/Netgifx/src/br/com/fiap/dao/impl/UsuarioDao.java
@@ -24,7 +24,7 @@ public class UsuarioDao extends DaoGenerico<Usuario>{
 	 * @return <code>true</code> se o usuário não existir e conseguir criar um novo, se não <code>false</code>
 	 */
 	public boolean cadastrarUsuario(Usuario usuario) {
-		if (this.buscarUsuario(usuario.getCpf()) == null) {
+		if (buscarUsuario(usuario.getApelido()) == null) {
 			super.adicionar(usuario);
 			return true;
 		}
