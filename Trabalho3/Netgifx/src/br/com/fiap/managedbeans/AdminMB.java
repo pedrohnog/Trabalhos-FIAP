8b5a030361d712c1f109fc605fd22104b553f4a0
Rafael Alves de Lima <rafael.alvesdelima89@gmail.com>
2017-09-11 21:53:43 -0300

    Criação do adminMB para validar se o usuário é admin

---
+++ Trabalho3/Netgifx/src/br/com/fiap/managedbeans/AdminMB.java
@@ -0,0 +1,25 @@
+package br.com.fiap.managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import br.com.fiap.entity.Usuario;
+
+@ManagedBean
+@RequestScoped
+public class AdminMB {	
+	
+	public boolean validarAdmin(){
+		HttpSession  httpSession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+		return httpSession != null && httpSession.getAttribute("usuario") != null && 
+				((Usuario)httpSession.getAttribute("usuario")).getAdmin();
+	}
+	
+	public String abrirPainel(){
+		System.out.println("Redirecionando pagina");
+		return "webapp/protected/admin/admin.xhtml?faces-redirect=true";
+	}
+
+}
